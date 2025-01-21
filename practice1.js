a803df4c50b80abbd0356d78d4d4fb2e35e22511
AnithaDaggula <anithareddy1215@gmail.com>
2025-01-21 22:26:34 +0530

    added practice1

---
+++ practice1.js
@@ -0,0 +1,11 @@
+let name="Anitha"
+let age="22"
+let isStudent="true"
+
+const gradyear="2024"
+
+
+console.log("Name:",name)
+console.log("Age:",age)
+console.log("Student Status:",isStudent)
+console.log("Graduation Year:",gradyear)
